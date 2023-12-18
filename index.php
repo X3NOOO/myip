2107b89e2dc32b2ffbc32683c2532d81732c1dbc
X3NO <X3NO@disroot.org>
2023-12-18 23:34:28 +0100

    First release

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+foreach(array('HTTP_CF_CONNECTING_IP', 'HTTP_X_FORWARDED_FOR', 'HTTP_X_FORWARDED', 'HTTP_FORWARDED', 'HTTP_CLIENT_IP', 'REMOTE_ADDR') as $key) {
+    if(array_key_exists($key, $_SERVER) === true && filter_var($_SERVER[$key], FILTER_VALIDATE_IP)) {
+        echo $_SERVER[$key];
+    }
+}
+?>
